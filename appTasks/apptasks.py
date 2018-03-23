316747a80b76e89acdeeb8c549d71ba4156beed9
yuelong.chen <yuelong.chen@yahoo.com>
2018-03-23 09:48:09 +0800

    加入path

--- appTasks/apptasks.py
+++ appTasks/apptasks.py
@@ -9,23 +9,36 @@
 # @Software: PyCharm
 
 from __future__ import absolute_import, unicode_literals
+import sys,os
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from monitor.celery import app
 import subprocess
 from monitor.utils import randomFileName
+from monitor.runshell import runshell
+
 
 
 @app.task
 def generateFileDirs():
-    file=randomFileName()
-    cmd='perl /home/PROJECTS/celery_monitor/tests/test.pl /home/PROJECTS/TMP/{file} {file}'.format(
+    file, _ = randomFileName({})
+    cmd = 'perl /home/PROJECTS/celery_monitor/tests/test.pl /home/PROJECTS/TMP/{file} {file}'.format(
         file=file
     )
-    # cmd='touch /home/gogogo.txt'
-    # cmd='mkdir -p /home/TMP/{}'.format(file)
-    p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p.communicate()
-    # return file
     return cmd
+
+
+@app.task
+def go_to_work(script, prefix, tmp='/tmp/'):
+    '''
+    提交脚本，并指明log输出前缀（.o,.e,.csv,.pdf）
+    :param script: 需要执行的脚本
+    :param prefix: 输出log的前缀
+    :return:
+    '''
+    osize, esize = runshell(script, prefix, tmp)
+    return osize
 
 
 def main():
